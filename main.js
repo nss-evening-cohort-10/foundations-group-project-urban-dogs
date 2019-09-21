5cda3682f1f47ec8bbc9458144da3e35fc534623
henok tessema <heniselie@gmail.com>
2019-09-21 13:53:46 -0500

    just check on my code

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-// console.log("hey guys"
-// );
-
-
-
 const upcomingConcert = [
     {   image: 'image/concert1.jpg',
         date: 'SEP 20, 2019',
@@ -50,12 +45,6 @@ const upcomingConcert = [
      }
 ];
 
-//creating pri- to-dom fun
-// const printToDom = (domString, divId)=>{  
-//     document.getElementById(divId).innerHTML = domString;
-// }
-
-
 const printToDom = (divId,textToPrint) => {
     const divContainer = document.getElementById(divId)
     divContainer.innerHTML = textToPrint
@@ -64,15 +53,16 @@ const printToDom = (divId,textToPrint) => {
 const upcomingConcertBuilder = (myArr)=> {
     let domString = '';
     for (let i=0; i < myArr.length; i++) {
-        domString += `
+        domString += 
+        `
      
-     <div class="heni">
+     <div class="main-container1">
         <div class="card" style="width: 350px;">
                 <img src="${myArr[i].image}" class="card-img-top" alt="...">
-                <div class="card-body">
-                <p class="card-title">${myArr[i].date}</p>
-                <p class="card-text">${myArr[i].venue}</p>
-                <h3>Rating</h3>
+                <div class="card-body dates-card-body">
+                <p class="card-title dates-card-title">${myArr[i].date}</p>
+                <p class="card-text dates-card-text">${myArr[i].venue}</p>
+                <h4>Rating</h4>
                 <p class="card-review">${myArr[i].review}</p>
                 <a href="https://www.google.com/search?q=concertthicket+for+sale&rlz=1C5CHFA_enUS860US863&oq=concertthicket+for+sale&aqs=chrome..69i57.7744j0j4&sourceid=chrome&ie=UTF-8&ibp=htl;events&rciv=evn&sa=X&ved=2ahUKEwjr3snm3N3kAhUCCawKHdmsCDQQ5rwDKAF6BAgSEAw#fpstate=tldetail&htidocid=pfQmZUVQKTbYCx3VJpA8-A%3D%3D&htivrt=events" class="btn btn-primary">Concert Tickets</a>
             </div>
